7306c0273114e9ab92d417cdf69ecadd0dd0555c
Muhammad Anwer <aamer684@gmail.com>
2025-06-24 14:35:01 -0400

    Instead of using BrowserRouter ,  use HashRouter

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-
+// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home/Home';
 import ReactDetail from './pages/technologies/ReactPage';
 import Angular from './pages/technologies/AngularPage';
